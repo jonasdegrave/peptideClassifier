9b79b8eb388cc45c352b3cdd5af2d7a96e3a1dd6
Jonas Degrave <jonasdegrave@poli.ufrj.br>
2021-11-27 22:27:36 -0300

    Major code rework in peptideClassifier

--- src/config.py
+++ src/config.py
@@ -11,8 +11,11 @@ TEMP_FILES_FOLDER = "../temp/"
 INPUT_FILES_FOLDER = "../inputFiles/"
 OUTPUT_FILES_FOLDER = "../outputFiles/"
 
-# TO-DO: REMOVE THIS
-INPUT_FILE = "proteinList.txt"
+# Prefix for result filename
+RESULT_FILENAME_PREFIX = "result_"
+
+# List of protein files to download
+DOWNLOAD_INDEX = "proteinList.txt"
 
 # Source URL to download protein files from
 SOURCE_URL = "https://www.uniprot.org/uniprot/"
@@ -25,3 +28,11 @@ MAXIMUM_THREADS = 32
 
 # Use cached files or download everything again
 DOWNLOAD_AGAIN = False
+
+# Protein name and peptide position column headers
+HEADER_PROTEIN = "protein"
+HEADER_P1 = "P1' position"
+
+# Protein keys to look for
+PEPTIDE_HEADER = "FT"
+PEPTIDE_KEYS = ["SIGNAL", "PROPEP"]
