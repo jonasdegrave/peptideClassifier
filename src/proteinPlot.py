b1d9ecb1cdf9cd8bfcf8a24ae607cb2cbd52d41c
Jonas Degrave <jonasdegrave@poli.ufrj.br>
2021-12-02 13:35:26 -0300

    Initial toughts on protein intervals for bar plot

--- src/proteinPlot.py
+++ src/proteinPlot.py
@@ -1,1 +1,14 @@
 # This routine plots proteins and peptides as horizontal bars highlighting regions of interest.
+from config import *
+import peptideClassifier
+import os
+
+fileName = "./temp/" + "P08572.txt"
+
+interval = peptideClassifier.getInterval (fileName)
+
+proteinSize = 1710
+
+
+
+print(interval)
