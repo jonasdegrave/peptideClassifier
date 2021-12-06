fdcdb16972a24b97c5003471e7ebe9b2e85b5f5b
Jonas Degrave <jonasdegrave@poli.ufrj.br>
2021-12-06 13:26:58 -0300

    proteinSegments aux function for protein plot

--- src/proteinPlot.py
+++ src/proteinPlot.py
@@ -6,18 +6,65 @@ import pandas as pd
 from os.path import expanduser as ospath
 
 
+def proteinSegments (M: int, N: int, peptides: list) -> list:
+    if peptides == []:
+        return [[PEPTIDE_OTHER, M, N]]
 
-fileName = "./temp/" + "P08572.txt"
+    peptide, a, b = peptides[0]
 
-sheetName= "/home/naiane/Documents/Doutorado/Software/peptideClassifier/inputFiles/" + "A375.xlsx"
+    ## Left-hand side
+    # If the protein starts with the peptide
+    if M == a:
+        protein = [[peptide, M, b]]
+    else:
+        protein = [[PEPTIDE_OTHER, M, a-1], [peptide, a, b]]
+
+    ## Right-hand side
+    # If the protein ends with the peptide
+    if N == b:
+        return protein
+    else:
+        return protein + proteinSegments(b+1, N, peptides[1:])
+ 
+# TESTS
+# print(proteinSegments(1, 10, [["SIGNAL", 3, 5], ["PROPEP", 7, 8]]))
+# print(proteinSegments(1, 473, [["SIGNAL", 51, 53], ["SIGNAL", 108, 120], ["PROPEP", 191, 234], ["PROPEP", 302, 415]]))
+
+fileName = TEMP_FILES_FOLDER + "P08572.txt"
+
+peptideSegments = peptideClassifier.getInterval(fileName)
+
+# proteinSize = getProteinSize(fileName)
+proteinSize = 1072
+
+proteinSegments = proteinSegments(1, proteinSize, peptideSegments)
+
+print()
+
+proteinSegments = [['OTHER', 1, 50],
+                    ['SIGNAL', 51, 53],
+                    ['OTHER', 54, 107],
+                    ['SIGNAL', 108, 120],
+                    ['OTHER', 121, 190],
+                    ['PROPEP', 191, 234],
+                    ['OTHER', 235, 301],
+                    ['PROPEP', 302, 415],
+                    ['OTHER', 416, 473]]
+
+
+def drawProtein (proteinSegments):
+    pass
+
+
+fileName = TEMP_FILES_FOLDER + "P08572.txt"
+
+sheetFileName = INPUT_FILES_FOLDER + "A375.xlsx"
 
 labels = ['PEPTIDE','PROPEP','SIGNAL']
-colors = ['#1D2F6F', '#8390FA', '#6EAF46']
-
-df=pd.read_excel(ospath(sheetName))
 
 
-def data(sheet):
+def proteinData(sheetFileName):
+    df=pd.read_excel(sheetFileName)
     field=["protein","Total Length","P1' position","Length Pep"]
     listProteinName=df[field[0]].values
     listProteinSize=df[field[1]].values
@@ -25,13 +72,13 @@ def data(sheet):
     listPepFinalRange=df[field[3]].values
     return  listProteinName,  listProteinSize, listPepInitialRange, listPepFinalRange
 
-data(df)
-
-
-interval = peptideClassifier.getInterval (fileName)
-
-proteinSize = 1710
+for protein in zip(proteinData(sheetFileName)):
+    proteinName, proteinSize, peptideInitialRange, peptideFinalRange = protein
 
 
 
-print(interval)
+# interval = peptideClassifier.getInterval (fileName)
+
+# proteinSize = 1710
+
+# print(interval)
